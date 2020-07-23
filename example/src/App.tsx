764b7d5c1829b3fca3c599835ca9ad5bf5c224a3
LyhourChhen <ukamilove66@gmail.com>
2020-07-23 16:44:26 +0700

    update

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -4,7 +4,7 @@ import { ExampleComponent } from 'antd-table-search'
 import 'antd-table-search/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <ExampleComponent text="Create React Library Mother fucker 😄" />
 }
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react'
 import styles from './styles.module.css'
+import 'antd/dist/antd.css'
+import { DatePicker } from 'antd';
 
 interface Props {
   text: string
 }
 
 export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+  return <div className={styles.test}>Example Component: {text}<div><DatePicker /></div></div>
 }
