c2b51291034d98d4ff0ebdf01d35615f5bd7e7cc
LyhourChhen <ukamilove66@gmail.com>
2020-07-23 16:48:45 +0700

    update

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -2,9 +2,14 @@ import React from 'react'
 
 import { ExampleComponent } from 'antd-table-search'
 import 'antd-table-search/dist/index.css'
-
+import { DatePicker } from 'antd'
 const App = () => {
-  return <ExampleComponent text="Create React Library Mother fucker 😄" />
+  return (
+    <div>
+      <ExampleComponent text='Create React Library Mother fucker 😄' />
+      <DatePicker />
+    </div>
+  )
 }
 
 export default App
