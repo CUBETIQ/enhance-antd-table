86a6bbb6c8aa570181ec5c7b7fe54e2654707d99
LyhourChhen <ukamilove66@gmail.com>
2020-07-24 15:01:27 +0700

    update library

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,80 +1,71 @@
 import React from 'react'
 import EnhanceAntdTable, { newColumnsInterface } from 'enhance-antd-table'
-import { Tag, Space } from 'antd'
-const columns: Array<newColumnsInterface> = [
-  {
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'name'
-    // render: (text: any) => <a>{text}</a>
-  },
-  {
-    title: 'Age',
-    dataIndex: 'age',
-    key: 'age'
-  },
-  {
-    title: 'Address',
-    dataIndex: 'address',
-    key: 'address'
-  },
-  {
-    title: 'Tags',
-    key: 'tags',
-    dataIndex: 'tags',
-    render: (tags: any) => (
-      <div>
-        {tags.map((tag: any) => {
-          let color = tag.length > 5 ? 'geekblue' : 'green'
-          if (tag === 'loser') {
-            color = 'volcano'
-          }
-          return (
-            <Tag color={color} key={tag}>
-              {tag.toUpperCase()}
-            </Tag>
-          )
-        })}
-      </div>
-    )
-  },
-  {
-    title: 'Action',
-    key: 'action',
-    render: (record: any) => (
-      <Space size='middle'>
-        <a>Invite {record.name}</a>
-        <a>Delete</a>
-      </Space>
-    )
-  }
-]
+import { Tag } from 'antd'
 
-const data = [
-  {
-    key: '1',
-    name: 'John Brown',
-    age: 32,
-    address: 'New York No. 1 Lake Park',
-    tags: ['nice', 'developer']
-  },
-  {
-    key: '2',
-    name: 'Jim Green',
-    age: 42,
-    address: 'London No. 1 Lake Park',
-    tags: ['loser']
-  },
-  {
-    key: '3',
-    name: 'Joe Black',
-    age: 32,
-    address: 'Sidney No. 1 Lake Park',
-    tags: ['cool', 'teacher']
-  }
-]
 
 const App = () => {
+
+  const columns: Array<newColumnsInterface> = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name'
+    },
+    {
+      title: 'Age',
+      dataIndex: 'age',
+      key: 'age'
+    },
+    {
+      title: 'Address',
+      dataIndex: 'address',
+      key: 'address'
+    },
+    {
+      title: 'Tags',
+      key: 'tags',
+      dataIndex: 'tags',
+      render: (tags: any) => (
+        <div>
+          {tags.map((tag: any) => {
+            let color = tag.length > 5 ? 'geekblue' : 'green'
+            if (tag === 'loser') {
+              color = 'volcano'
+            }
+            return (
+              <Tag color={color} key={tag}>
+                {tag.toUpperCase()}
+              </Tag>
+            )
+          })}
+        </div>
+      )
+    }
+  ]
+
+  const data = [
+    {
+      key: '1',
+      name: 'John Brown',
+      age: 32,
+      address: 'New York No. 1 Lake Park',
+      tags: ['nice', 'developer']
+    },
+    {
+      key: '2',
+      name: 'Jim Green',
+      age: 42,
+      address: 'London No. 1 Lake Park',
+      tags: ['loser']
+    },
+    {
+      key: '3',
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park',
+      tags: ['cool', 'teacher']
+    }
+  ]
   return (
     <div>
       <div
--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,57 @@
 import 'antd/dist/antd.css'
-import { Table } from 'antd'
+import { Button, Dropdown, Menu, Space, Table } from 'antd'
 import { TableProps, ColumnProps } from 'antd/es/table'
 import React from 'react'
+import { DownOutlined, UserOutlined } from '@ant-design/icons/lib'
 
 interface enhanceTableInterface<IRowData = any> extends TableProps<IRowData> {
   newColumns?: Array<newColumnsInterface>
   newSources?: Array<any>
 }
 
-export interface newColumnsInterface<T = any> extends ColumnProps<T> {}
+export interface newColumnsInterface<T = any> extends ColumnProps<T> {
+}
 
 const EnhanceAntdTable: React.FC<enhanceTableInterface> = (props) => {
+  const menu = (
+    <Menu>
+      <Menu.Item key='1' icon={<UserOutlined/>}>
+        1st menu item
+      </Menu.Item>
+      <Menu.Item key='2' icon={<UserOutlined/>}>
+        2nd menu item
+      </Menu.Item>
+      <Menu.Item key='3' icon={<UserOutlined/>}>
+        3rd item
+      </Menu.Item>
+    </Menu>
+  )
+
+  const defaultColumns: Array<newColumnsInterface> = [
+    ...(props.newColumns || []),
+    {
+      title: 'Action',
+      key: 'action',
+      render: () => (
+        <Space size='middle'>
+
+          <Dropdown overlay={menu}>
+            <Button>
+              Button <DownOutlined/>
+            </Button>
+          </Dropdown>
+        </Space>
+      )
+    }
+  ]
   return (
-    <Table
-      bordered={props.bordered}
-      dataSource={props.newSources}
-      columns={props.newColumns}
-    />
+    <div>
+      <Table
+        bordered={props.bordered}
+        dataSource={props.newSources}
+        columns={defaultColumns}
+      />
+    </div>
   )
 }
 
