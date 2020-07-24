3df43706f8887e5f7bbf6a123280992834071d29
LyhourChhen <ukamilove66@gmail.com>
2020-07-24 13:38:32 +0700

    move library to index.ts

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,5 +1,79 @@
 import React from 'react'
-import { EnhanceAntdTable } from 'enhance-antd-table'
+import EnhanceAntdTable, { newColumnsInterface } from 'enhance-antd-table'
+import { Tag, Space } from 'antd'
+const columns: Array<newColumnsInterface> = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name'
+    // render: (text: any) => <a>{text}</a>
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age'
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address'
+  },
+  {
+    title: 'Tags',
+    key: 'tags',
+    dataIndex: 'tags',
+    render: (tags: any) => (
+      <div>
+        {tags.map((tag: any) => {
+          let color = tag.length > 5 ? 'geekblue' : 'green'
+          if (tag === 'loser') {
+            color = 'volcano'
+          }
+          return (
+            <Tag color={color} key={tag}>
+              {tag.toUpperCase()}
+            </Tag>
+          )
+        })}
+      </div>
+    )
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    render: (record: any) => (
+      <Space size='middle'>
+        <a>Invite {record.name}</a>
+        <a>Delete</a>
+      </Space>
+    )
+  }
+]
+
+const data = [
+  {
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+    tags: ['nice', 'developer']
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+    tags: ['loser']
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+    tags: ['cool', 'teacher']
+  }
+]
+
 const App = () => {
   return (
     <div>
@@ -11,7 +85,11 @@ const App = () => {
           height: '100vh'
         }}
       >
-        <EnhanceAntdTable text={'EnhandAntTable 😄'} />
+        <EnhanceAntdTable
+          bordered={true}
+          newColumns={columns}
+          newSources={data}
+        />
       </div>
     </div>
   )
--- src/index.tsx
+++ src/index.tsx
@@ -1,93 +1,27 @@
-import * as React from 'react'
-import styles from './styles.module.css'
-import { Tag, Space } from 'antd'
 import 'antd/dist/antd.css'
-import NewTable, { newColumnsInterface } from './NewTable'
-interface Props {
-  text: string
+import { Table } from 'antd'
+import { TableProps, ColumnProps } from 'antd/es/table'
+import React from 'react'
+
+interface enhanceTableInterface<IRowData = any> extends TableProps<IRowData> {
+  newColumns?: Array<newColumnsInterface>
+  newSources?: Array<any>
 }
 
-const columns: Array<newColumnsInterface> = [
-  {
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'name'
-    // render: (text: any) => <a>{text}</a>
-  },
-  {
-    title: 'Age',
-    dataIndex: 'age',
-    key: 'age'
-  },
-  {
-    title: 'Address',
-    dataIndex: 'address',
-    key: 'address'
-  },
-  {
-    title: 'Tags',
-    key: 'tags',
-    dataIndex: 'tags',
-    render: (tags: any) => (
-      <div>
-        {tags.map((tag: any) => {
-          let color = tag.length > 5 ? 'geekblue' : 'green'
-          if (tag === 'loser') {
-            color = 'volcano'
-          }
-          return (
-            <Tag color={color} key={tag}>
-              {tag.toUpperCase()}
-            </Tag>
-          )
-        })}
-      </div>
-    )
-  },
-  {
-    title: 'Action',
-    key: 'action',
-    render: (record: any) => (
-      <Space size='middle'>
-        <a>Invite {record.name}</a>
-        <a>Delete</a>
-      </Space>
-    )
-  }
-]
+export interface newColumnsInterface<T = any> extends ColumnProps<T> {}
 
-const data = [
-  {
-    key: '1',
-    name: 'John Brown',
-    age: 32,
-    address: 'New York No. 1 Lake Park',
-    tags: ['nice', 'developer']
-  },
-  {
-    key: '2',
-    name: 'Jim Green',
-    age: 42,
-    address: 'London No. 1 Lake Park',
-    tags: ['loser']
-  },
-  {
-    key: '3',
-    name: 'Joe Black',
-    age: 32,
-    address: 'Sidney No. 1 Lake Park',
-    tags: ['cool', 'teacher']
-  }
-]
-
-export const EnhanceAntdTable = ({ text }: Props) => {
+const EnhanceAntdTable: React.FC<enhanceTableInterface> = (props) => {
   return (
-    <div>
-      <div className={styles.test}>
-        Example Component: <b>{text}</b>
-        <hr />
-        <NewTable bordered={true} newColumns={columns} newSources={data} />
-      </div>
-    </div>
+    <Table
+      bordered={props.bordered}
+      dataSource={props.newSources}
+      columns={props.newColumns}
+    />
   )
 }
+
+EnhanceAntdTable.defaultProps = {
+  bordered: true
+}
+
+export default EnhanceAntdTable
