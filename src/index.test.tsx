278cbd31325836380562c23934ef8e87f32d5233
LyhourChhen <ukamilove66@gmail.com>
2020-07-24 12:01:08 +0700

    update column interface

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,7 +1,7 @@
-import { ExampleComponent } from '.'
+import { EnhanceAntdTable } from '.'
 
 describe('ExampleComponent', () => {
   it('is truthy', () => {
-    expect(ExampleComponent).toBeTruthy()
+    expect(EnhanceAntdTable).toBeTruthy()
   })
 })
