7841d6f226f37015b283905b57cbb6a88fc3028e
LyhourChhen <ukamilove66@gmail.com>
2020-07-28 10:19:50 +0700

    delete travia.ci, enable eslint, refactor somecode

--- src/components/actionMenu.tsx
+++ src/components/actionMenu.tsx
@@ -1,50 +1,50 @@
 import { Button, Dropdown, Menu, Space } from 'antd'
-import { DeleteOutlined, DownOutlined, ExpandOutlined } from '@ant-design/icons/lib'
+import {
+  DeleteOutlined,
+  DownOutlined,
+  ExpandOutlined
+} from '@ant-design/icons/lib'
 import { v4 as uuid } from 'uuid'
 import { MenuProps } from 'antd/es/menu'
 import React from 'react'
 
-export interface actionMenuPropsInterface extends MenuProps {
-}
+export interface actionMenuPropsInterface extends MenuProps {}
 
 interface ActionMenuInterface {
-  delete?: any,
-  detail?: any,
-  renderNew?: any,
+  delete?: any
+  detail?: any
+  renderNew?: any
 }
 
 const ActionMenu: React.FC<ActionMenuInterface> = (props) => {
-  console.log("props render", props.renderNew)
+  console.log('props render', props.renderNew)
   const menu = () => (
     <div>
-      {props.renderNew === undefined ?
+      {props.renderNew === undefined ? (
         <Menu>
-          <Menu.Item key={uuid()} icon={<ExpandOutlined/>} {...props.detail}>
+          <Menu.Item key={uuid()} icon={<ExpandOutlined />} {...props.detail}>
             Detail
           </Menu.Item>
-          <Menu.Item key={uuid()} icon={<DeleteOutlined/>} {...props.delete}>
+          <Menu.Item key={uuid()} icon={<DeleteOutlined />} {...props.delete}>
             Delete
           </Menu.Item>
         </Menu>
-        : <div>
-          {props.renderNew}
-        </div>}
+      ) : (
+        <div>{props.renderNew}</div>
+      )}
     </div>
   )
   return (
     <div>
-      <div>
-      </div>
       <Space size='middle'>
         <Dropdown overlay={menu}>
           <Button>
-            More <DownOutlined/>
+            More <DownOutlined />
           </Button>
         </Dropdown>
       </Space>
     </div>
   )
 }
-
 
 export default ActionMenu
