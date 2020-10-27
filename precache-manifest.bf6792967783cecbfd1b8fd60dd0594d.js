12973c3a7ec2c9f6a52e6a0fdcfc38f62a308a5b
vutpov <vutpov@cubetiqs.com>
2020-10-27 15:42:11 +0700

    Updates

---
+++ precache-manifest.bf6792967783cecbfd1b8fd60dd0594d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be777309a9acbed3dde9",
+    "url": "build/2.be777309.js"
+  },
+  {
+    "revision": "baa4a240b0b42c576d0306a6cda8e15f",
+    "url": "build/2.be777309.js.LICENSE.txt"
+  },
+  {
+    "revision": "e46008162eaa4d8d14fe",
+    "url": "build/bundle.e4600816.js"
+  },
+  {
+    "revision": "963f478d1a8cc8c33e33",
+    "url": "build/main.963f478d.js"
+  },
+  {
+    "revision": "6041886772b6e7506a095dcd68d48a86",
+    "url": "index.html"
+  },
+  {
+    "revision": "be777309a9acbed3dde9",
+    "url": "static/css/2.9c8c8187.chunk.css"
+  }
+]);
