0b209bb3f523f2a2615ec679afc5a40872f13d0d
vutpov <vutpov@cubetiqs.com>
2020-11-05 10:30:05 +0700

    update

--- src/index.tsx
+++ src/index.tsx
@@ -189,7 +189,7 @@ const EnhanceAntdTable: React.FC<enhanceTableInterface> = (props) => {
           marginBottom: 10,
           flexWrap: 'wrap'
         }}
-        className={headerClassName}
+        className={headerClassName + ' e-tableHeader'}
       >
         <Space
           style={{
